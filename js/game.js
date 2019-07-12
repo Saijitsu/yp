3a10837a6e614d9848ba03dda59686c65dd26101
remisimaer <remi.simaer@gmail.com>
2019-07-12 14:17:57 +0200

    semi-colon deleted

--- js/game.js
+++ js/game.js
@@ -1,27 +1,25 @@
 // Start the game:
-document.addEventListener('DOMContentLoaded', ()=> {
-    document.getElementById('start-button').addEventListener('click', () => {
-        document.querySelector('.board-and-players-info').style.display = 'flex';
-        document.querySelector('.game-option-rules').style.display = 'none';
-        document.querySelector('#duel').style.display = 'none';
-        document.querySelector('#victory').style.display = 'none';
-        userDefinedSettings();
-        // Who starting the game? Select the player Number
-        let playerNumber = Math.floor(Math.random() * Math.floor(numbersOfPlayers));
-        currentPlayer = players[playerNumber]; //Selected starting Player
-        currentPlayerIs();
-        createRandomCellList();
-        boardCreation(); //create the board
-        // Modify CSS elements
-        let elmt = document.getElementById("canvas");
-        // Modify style
-        elmt.style.background = getGradiantBackground();
-        elmt.style.border = "1px solid #EEC965";
-        elmt.width = width;
-        elmt.height = height;
-        draw();
-        currentPlayer.tripArea();
-        updateStatistics();
-        changeTrack(adventureMusic);
-    });
+document.getElementById('start-button').addEventListener('click', () => {
+  document.querySelector('.board-and-players-info').style.display = 'flex';
+  document.querySelector('.game-option-rules').style.display = 'none';
+  document.querySelector('#duel').style.display = 'none';
+  document.querySelector('#victory').style.display = 'none';
+  userDefinedSettings();
+  // Who starting the game? Select the player Number
+  let playerNumber = Math.floor(Math.random() * Math.floor(numbersOfPlayers));
+  currentPlayer = players[playerNumber]; //Selected starting Player
+  currentPlayerIs();
+  createRandomCellList();
+  boardCreation(); //create the board
+  // Modify CSS elements
+  let elmt = document.getElementById("canvas");
+  // Modify style
+  elmt.style.background = getGradiantBackground();
+  elmt.style.border = "1px solid #EEC965";
+  elmt.width = width;
+  elmt.height = height;
+  draw();
+  currentPlayer.tripArea();
+  updateStatistics();
+  changeTrack(adventureMusic);
 });
--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Game settings
 let numbersOfPlayers = 2,
-    obstacleCell = null;
+    obstacleCell = null,
     chestCell = null,
     highLightning = [],
     boardSize = null,
